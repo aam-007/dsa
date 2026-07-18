e42d2d0b2bf8c1c90d7199671f91f97db3824d5c
aam-007 <adityamishra2007nsk@gmail.com>
2026-07-18 21:06:56 +0530

    added files

--- arrays/cpp/1traversal.cpp
+++ arrays/cpp/1traversal.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
 using namespace std;
 
-void trav(int arr[], int lb, int ub);
-
-int main()
-{
-    int arr[] = {1,2,3, 4};
-    int lb = 0;
-
-    int n = sizeof(arr)/sizeof(arr[0]);
-
-    int ub = n-1;
-
-    trav(arr, lb, ub); 
-
-    return 0 ;
-}
-
 void trav(int arr[], int lb, int ub)
 {
-    for(int k=lb; k<=ub; k++)
+    for (int k = lb; k<=ub; k++)
     {
         cout << arr[k] << '\t';
     }
 
     cout << endl;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 4};
+    int lb{0};
+    int n = sizeof(arr)/sizeof(arr[0]); // len of arr
+
+    int ub = n-1; 
+
+    trav(arr, lb, ub);
+
+    return 0; 
 }
