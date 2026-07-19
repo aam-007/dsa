16656e043b32235a35c2697e7039ecb49a7c12ca
aam-007 <adityamishra2007nsk@gmail.com>
2026-07-19 12:37:11 +0530

    added files

--- arrays/cpp/1traversal.cpp
+++ arrays/cpp/1traversal.cpp
@@ -1,25 +1,25 @@
+// travese an arr
+
 #include <iostream>
 using namespace std;
 
 void trav(int arr[], int lb, int ub)
 {
-    for (int k = lb; k<=ub; k++)
-    {
+    for (int k = lb; k<=ub; k++){
         cout << arr[k] << '\t';
     }
-
-    cout << endl;
 }
 
 int main()
 {
-    int arr[] = {1, 2, 4};
-    int lb{0};
-    int n = sizeof(arr)/sizeof(arr[0]); // len of arr
+    int arr [] = {1, 2, 8, 9};
+    int lb = 0;
+    int len = sizeof(arr)/sizeof(arr[0]);
 
-    int ub = n-1; 
+    int ub = len - 1;
 
-    trav(arr, lb, ub);
+    trav(arr, lb, ub); 
 
     return 0; 
+
 }
--- arrays/cpp/2insertion.cpp
+++ arrays/cpp/2insertion.cpp
@@ -1,3 +1,60 @@
+// insert element 
+
 #include <iostream>
 using namespace std;
 
+void insert(int arr[], int &n, int cap, int pos, int item){
+    // check overflow
+
+    if (n>cap){
+        cout << "overflow";
+    }
+
+    // check valid pos
+
+    if (pos < 0 || pos > n)
+    {
+        cout << "invaid pos"; 
+    }
+
+    // shiftig elements 
+
+    for (int k =n; k > pos; k--){
+        arr[k] = arr[k-1];
+
+
+    }
+
+    //insert element
+    arr[pos] = item; cout << "\n";
+
+    //inc size of array
+
+    n++; 
+}
+
+int main()
+{
+    int arr[10] = {1, 2, 3, 4};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int cap = 10;
+    int pos = 2; 
+    int item; 
+    cout << "Enter element to insert: "; cin >> item; 
+
+    cout << "Elements before insertion: " << '\n';
+
+    for (int i =0; i <= n-1; i++){
+        cout << arr[i] << '\t';
+    }
+
+    insert(arr, n, cap, pos, item);
+    cout << "After insertion: " << '\n';
+
+    
+    for (int i =0; i <= n-1; i++){
+        cout << arr[i] << '\t';
+    }
+    
+
+}
