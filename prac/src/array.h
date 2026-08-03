00572e2863238241c354200b1186e05e054c6338
Aditya Mishra <103280156+aam-007@users.noreply.github.com>
2026-08-03 09:05:46 +0530

    Delete prac directory

--- prac/src/array.h
+++
@@ -1,9 +0,0 @@
-#ifndef ARRAY_H
-#define ARRAY_H
-
-// Function declarations
-void printArray(int arr[], int size);
-int sumArray(int arr[], int size);
-int maxArray(int arr[], int size);
-
-#endif // ARRAY_H
