5056bb6cbc3ae26a036b0f8a604e02fe50174fb0
aam-007 <adityamishra2007nsk@gmail.com>
2026-08-04 21:45:09 +0530

    af

--- rev/2dArrays.cpp
+++ rev/2dArrays.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
 
 namespace Mat{
-    const int ROWS = 3;
-    const int COLS = 3;
+    constexpr int ROWS = 3;
+    constexpr int COLS = 3;
 
     struct Matrix{
-        int arr[ROWS][COLS];
+        int arr[ROWS][COLS] {};
     };
 
     void disp(const Matrix &m){
-        std :: cout << "Zero initialized matrix: " << '\n';
         for (int i=0; i<ROWS; ++i){
             for (int k=0; k<COLS; ++k){
                 std :: cout << m.arr[i][k] << " ";
             }
-            std :: cout << "\n" ;
+            std :: cout << '\n' ;
         }
     }
 
 }
 
 int main(){
-    Mat :: Matrix m{}; 
-    Mat :: disp(m); 
+    Mat :: Matrix matrix {}; 
+    std :: cout << "Zero initialized matrix: " << '\n';
+    Mat :: disp(matrix); 
 
     return 0; 
 }
