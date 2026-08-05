88b25550c4d5df80f6383d3b79d62452683a740c
aam-007 <adityamishra2007nsk@gmail.com>
2026-08-05 14:01:42 +0530

    modified:   rev/stacks.cpp

--- rev/stacks.cpp
+++ rev/stacks.cpp
@@ -1,4 +1,39 @@
 #include <iostream>
-using namespace std;
 
-void 
+namespace Stacks {
+    constexpr int MAX {100};
+
+    struct stack{
+        int arr[MAX];
+        int top{-1};
+        int item;
+    };
+
+    bool isOverFlow(const stack &s){
+        return (s.top==MAX-1);
+    }
+    bool isUnderFlow(const stack &s){
+        return (s.top==-1);
+    }
+
+    void push(stack &s){
+        if (isOverFlow(s)){
+            std::cout << "Overflow!";
+        }
+
+        ++s.top;
+        s.arr[s.top] = s.item;
+        std::cout << "Element added to stack";
+
+    }
+
+    int pop(stack &s){
+        if (isUnderFlow(s)){
+            return -1;
+        }
+
+        int poppedItem{s.top};
+        --s.top; 
+        return poppedItem;
+    }
+}
