537e89ad5a5ce71df9e5584e265a3bba9b3af7c2
aam-007 <adityamishra2007nsk@gmail.com>
2026-08-11 22:20:32 +0530

    af

---
+++ queue/circularQueues.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstddef>
+
+namespace lds{
+    constexpt std::size_t MAX {10};
+    enum class STATUS{
+        OVERFLOW,
+        UNDERFLOW,
+        OK
+    };
+
+    struct circularQueue{
+
+    };
+}
+//to finish
